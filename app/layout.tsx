3b884f2f8bb69a8e8dbad8706791d5a9882ba9db
J055-B <jos.b@callistogroup.org>
2026-08-04 19:03:16 +0300

    Fix globals.css import path

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import '../styles/globals.css'
 import React from 'react'
 import Sidebar from '../components/layout/Sidebar'
 import Header from '../components/layout/Header'
