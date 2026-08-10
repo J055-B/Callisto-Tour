62cc9c351c641cd7557b8d5b284a9819af0da987
J055-B <jos.b@callistogroup.org>
2026-08-10 20:37:03 +0300

    Add files via upload

--- lib/music.ts
+++ lib/music.ts
@@ -8,5 +8,18 @@ export const PLAYLIST: string[] = [
   '/audio/playlist/run-you.mp3', // Run You - The Qemists
   '/audio/playlist/cant-keep-me-down.mp3', // Can't Keep Me Down (feat. Sena & Shystie) - Murdock
   '/audio/playlist/crush.mp3', // Crush - Pendulum
-  '/audio/playlist/the-spark.mp3' // The Spark (feat. Lisdoonvarna Crew) - Kabin Crew
+  '/audio/playlist/the-spark.mp3', // The Spark (feat. Lisdoonvarna Crew) - Kabin Crew
+  '/audio/playlist/the-wonders-of-you.mp3', // The Wonders Of You - Andy Hunter
+  '/audio/playlist/black-betty.mp3', // Black Betty
+  '/audio/playlist/rush-hour.mp3', // Rush Hour
+  '/audio/playlist/rockstar.mp3', // Rockstar
+  '/audio/playlist/riders-on-the-storm.mp3', // Riders On The Storm (Fredwreck Remix feat. The Doors) - Snoop Dogg
+  '/audio/playlist/gas-gas-gas.mp3', // Gas Gas Gas - Manuel
+  '/audio/playlist/under-the-knife.mp3', // Under The Knife - Rise Against
+  '/audio/playlist/to-hell-we-ride.mp3', // To Hell We Ride - Lostprophets
+  '/audio/playlist/swallow.mp3', // Swallow - Blindside
+  '/audio/playlist/two-lane-blacktop.mp3', // Two-Lane Blacktop - Rob Zombie
+  '/audio/playlist/tokyo-drift.mp3', // Tokyo Drift
+  '/audio/playlist/deja-vu.mp3', // Deja Vu (2019) - Dave Rodgers
+  '/audio/playlist/running-in-the-90s.mp3' // Running in the 90's
 ]
